1d7921aa2161d23a2942d0e84d9e8b50fe52e00a
Jon Petraglia <notbryant@gmail.com>
2014-07-07 22:31:18 -0600

    Added actual Callisto icons

--- src/com/qweex/callisto/MasterActivity.java
+++ src/com/qweex/callisto/MasterActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
