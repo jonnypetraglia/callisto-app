e6789de3e1c4a4d24e1631a27cf4fbfebb23d25f
Jon Petraglia <notbryant@gmail.com>
2014-07-11 03:55:29 -0600

    Commenting: CallistoFragment

--- src/com/qweex/callisto/CallistoFragment.java
+++ src/com/qweex/callisto/CallistoFragment.java
@@ -2,13 +2,20 @@ package com.qweex.callisto;
 
 import android.support.v4.app.Fragment;
 
+/** Base Fragment that includes some niceties.
+ * @author      Jon Petraglia <notbryant@gmail.com>
+ */
 public abstract class CallistoFragment extends Fragment {
 
+    /** Constructor; supplies MasterActivity reference. */
     public CallistoFragment(MasterActivity master) {
         this.master = master;
     }
 
+    /** MasterActivity reference. */
     protected MasterActivity master;
+
+    /** Abstract methods. */
     public abstract void show();
     public abstract void hide();
 }
