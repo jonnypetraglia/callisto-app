0b2c4372aee0a1f1038a65e03b88dbfcc24cb83c
Jon Petraglia <notbryant@gmail.com>
2014-07-11 02:25:50 -0600

    Commenting: CatalogAdapter

--- src/com/qweex/callisto/catalog/CatalogAdapter.java
+++ src/com/qweex/callisto/catalog/CatalogAdapter.java
@@ -2,26 +2,34 @@ package com.qweex.callisto.catalog;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import com.qweex.callisto.R;
 
-import java.io.File;
-import java.text.ParseException;
-import java.util.Date;
-
+/**
+ * This is an adapter for CatalogFragment. It manages moving data from Sqlite to the views in the ListView.
+ *
+ * @author      Jon Petraglia <notbryant@gmail.com>
+ */
 public class CatalogAdapter extends SimpleCursorAdapter
 {
+    /** The cursor pointing at the data in the database. */
     private Cursor cursor;
+    /** The context to use for the LayoutInfater. */
     private Context context;
+    /** The R.layout id to inflate for each item. */
     private int layout_id;
 
+    /**
+     * Inherited constructor.
+     *
+     * @param context The context to use for the LayoutInfater.
+     * @param layout The R.layout id to inflate for each item.
+     * @param cursor The cursor pointing at the data in the database.
+     */
     public CatalogAdapter(Context context, int layout, Cursor cursor) {
         super(context, layout, cursor, new String[] {}, new int[] {});
         this.cursor = cursor;
@@ -29,6 +37,14 @@ public class CatalogAdapter extends SimpleCursorAdapter
         this.context = context;
     }
 
+    /**
+     * Inherited method to generate / recycle the view & inject data from relevant data row.
+     *
+     * @param pos Position of the item in question of the collection.
+     * @param inView View passed in that might be recycled.
+     * @param parent ViewGroup that should be used as parent for inView.
+     * @return
+     */
     public View getView(int pos, View inView, ViewGroup parent)
     {
         View v = inView;
@@ -47,104 +63,6 @@ public class CatalogAdapter extends SimpleCursorAdapter
         title.setText(cursor.getString(cursor.getColumnIndex("title")));
         date.setText(cursor.getString(cursor.getColumnIndex("date")));
 
-        /*
-        Date tempDate = new Date(); //We use this variable to get thisYear as well as parsing the actual date later
-        int thisYear = tempDate.getYear();      //If the date for the show is this year, no need to show the year
-        //Set the data From->To
-
-        //Get info for selected episode
-        long id = this.c.getLong(this.c.getColumnIndex("_id"));
-        this.c = StaticBlob.databaseConnector.getOneEpisode(id);
-        this.c.moveToFirst();
-        String date = this.c.getString(this.c.getColumnIndex("date"));
-        String title = this.c.getString(this.c.getColumnIndex("title"));
-        String mp3_link = this.c.getString(this.c.getColumnIndex("mp3link"));
-        String vid_link = this.c.getString(this.c.getColumnIndex("vidlink"));
-
-        //_id
-        ((TextView) v.findViewById(R.id.hiddenId)).setText(Long.toString(id));
-        //title
-        ((TextView) v.findViewById(R.id.rowTextView)).setText(title);
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams)v.findViewById(R.id.rowTextView).getLayoutParams();
-        lp.setMargins((int)(10*StaticBlob.DP), lp.topMargin, lp.rightMargin, lp.bottomMargin);;
-        //date
-        String d = date;
-        try {
-            tempDate = StaticBlob.sdfRaw.parse(d);
-            if(tempDate.getYear()==thisYear)
-                d = StaticBlob.sdfHuman.format(tempDate);
-            else
-                d = StaticBlob.sdfHumanLong.format(tempDate);
-            //d = Callisto.sdfDestination.format();
-        } catch (ParseException e) {
-            Log.e(TAG + ":ParseException", "Error parsing a date from the SQLite db: ");
-            Log.e(TAG+":ParseException", d);
-            Log.e(TAG+":ParseException", "(This should never happen).");
-            e.printStackTrace();
-        }
-        ((TextView) v.findViewById(R.id.rowSubTextView)).setText(d);
-
-        File music_file_location = null, video_file_location = null;
-        try {
-            music_file_location = new File(StaticBlob.storage_path + File.separator + currentShow);
-            music_file_location = new File(music_file_location, StaticBlob.sdfFile.format(tempDate) + "__" + StaticBlob.makeFileFriendly(title) + EpisodeDesc.getExtension(mp3_link));
-        }catch(NullPointerException npe) {
-            Log.e(TAG, "Null pointer when determining file status: Audio");
-        }
-        try {
-            video_file_location = new File(StaticBlob.storage_path + File.separator + currentShow);
-            video_file_location = new File(video_file_location, StaticBlob.sdfFile.format(tempDate) + "__" + StaticBlob.makeFileFriendly(title) + EpisodeDesc.getExtension(vid_link));
-        }catch(NullPointerException npe) {
-            Log.e(TAG, "Null pointer when determining file status: Video");
-        }
-
-        runOnUiThread(new updateBoldOrItalic(id, v, music_file_location, video_file_location, this.c.getLong(this.c.getColumnIndex("mp3size")), this.c.getLong(this.c.getColumnIndex("vidsize"))));
-
-
-        // Mark current queue item
-        Cursor c = StaticBlob.databaseConnector.currentQueueItem();
-        if(currentQueueItem==null || currentQueueItem == v)
-        {
-            if(c.getCount()>0)
-            {
-                c.moveToFirst();
-                if(c.getLong(c.getColumnIndex("identity"))==id)
-                    currentQueueItem = v;
-                else
-                    currentQueueItem = null;
-            }
-            else
-                currentQueueItem = null;
-        }
-        // Mark current download item
-        c = StaticBlob.databaseConnector.getActiveDownloads();
-        if(currentDownloadItem==null || currentDownloadItem == v)
-        {
-            if(c.getCount()>0)
-            {
-                c.moveToFirst();
-                Log.i(TAG, "  currentDownloadItem =? " + currentDownloadItem + "   " + c.getLong(c.getColumnIndex("identity")) + " == " + id);
-                if(c.getLong(c.getColumnIndex("identity"))==id)
-                    currentDownloadItem = v;
-                else
-                    currentDownloadItem = null;
-            }
-            else
-                currentDownloadItem = null;
-        }
-        Log.i(TAG, "currentDownloadItem == " + currentDownloadItem);
-
-        //Hide the specific views
-        int[] hide = new int[] { R.id.remove, R.id.progress, R.id.grabber, R.id.rightTextView};
-        for(int i=0; i<hide.length; i++)
-            v.findViewById(hide[i]).setVisibility(View.GONE);
-
-        //Check the Jupiter icon if it is new
-        boolean is_new = this.c.getInt(this.c.getColumnIndex("new"))>0;
-        CheckBox rb = ((CheckBox)v.findViewById(R.id.img));
-        rb.setChecked(is_new);
-        rb.setOnCheckedChangeListener(toggleNew);
-//*/
         return(v);
     }
 }
