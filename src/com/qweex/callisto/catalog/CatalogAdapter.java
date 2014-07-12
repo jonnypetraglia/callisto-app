931c4b1d68bb53180e29b98fdd19ca53c1176e8e
Jon Petraglia <notbryant@gmail.com>
2014-07-12 00:50:45 -0600

    Cleanification of Catalog Rows

--- src/com/qweex/callisto/catalog/CatalogAdapter.java
+++ src/com/qweex/callisto/catalog/CatalogAdapter.java
@@ -9,11 +9,22 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import com.qweex.callisto.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /** This is an adapter for CatalogFragment. It manages moving data from Sqlite to the views in the ListView.
  * @author      Jon Petraglia <notbryant@gmail.com>
  */
 public class CatalogAdapter extends SimpleCursorAdapter
 {
+    /** Date formatter for Listview */
+    public static final SimpleDateFormat sdfHuman = new SimpleDateFormat("MMM d");
+    public static final SimpleDateFormat sdfHumanLong = new SimpleDateFormat("MMM d, yyyy");
+    public static final SimpleDateFormat sdfWeekday = new SimpleDateFormat("EEE", Locale.US);
+
     /** The cursor pointing at the data in the database. */
     private Cursor cursor;
     /** The context to use for the LayoutInfater. */
@@ -55,8 +66,42 @@ public class CatalogAdapter extends SimpleCursorAdapter
                  date  = (TextView) v.findViewById(R.id.date);
 
         title.setText(cursor.getString(cursor.getColumnIndex("title")));
-        date.setText(cursor.getString(cursor.getColumnIndex("date")));
+        date.setText(formatDate(cursor.getString(cursor.getColumnIndex("date"))));
 
         return(v);
+    }
+
+    String formatDate(String input) {
+        try {
+            Date d = DatabaseMate.sdfRaw.parse(cursor.getString(cursor.getColumnIndex("date")));
+            Calendar cal = Calendar.getInstance(), tester = Calendar.getInstance();
+            cal.setTime(d);
+
+            tester.set(Calendar.HOUR_OF_DAY, 0);
+            tester.set(Calendar.MINUTE, 0);
+            tester.set(Calendar.SECOND, 0);
+            tester.set(Calendar.MILLISECOND, 0);
+
+            // Today @ {time}
+            if(cal.after(tester))
+                return "" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + " " + cal.get(Calendar.AM_PM);
+            // Yesterday
+            tester.add(Calendar.DATE, -1);
+            if(cal.after(tester))
+                return "Yesterday";
+            // {in the last week}day
+            tester.add(Calendar.DATE, -6);
+            if(cal.after(tester)) {
+                return sdfWeekday.format(cal.getTime());
+            }
+            // this year
+            if(cal.get(Calendar.YEAR)==Calendar.getInstance().get(Calendar.YEAR))
+                return sdfHuman.format(cal.getTime());
+            // earlier than this year
+            return sdfHumanLong.format(cal.getTime());
+
+        } catch (ParseException e) {
+            return "[ERROR]";
+        }
     }
 }
